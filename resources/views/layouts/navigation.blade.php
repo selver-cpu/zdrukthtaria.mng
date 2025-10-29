443c858cb103ae3f813bad46f4ba7afc230e6c01
Veli <veli@example.com>
2025-10-29 20:45:34 +0100

    Add Optimizimi button to mobile hamburger menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -139,6 +139,12 @@
             <x-responsive-nav-link :href="route('raportet.index')" :active="request()->routeIs('raportet.*')" class="touch-target touch-ripple touch-active">
                 {{ __('Raportet') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('cutting-optimization.index')" :active="request()->routeIs('cutting-optimization.*')" class="touch-target touch-ripple touch-active flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
+                </svg>
+                {{ __('Optimizimi') }}
+            </x-responsive-nav-link>
             {{-- Moduli i planifikimit të rafteve u hoq --}}
             @can('view-ditar')
             <x-responsive-nav-link :href="route('ditar.index')" :active="request()->routeIs('ditar.index')" class="touch-target touch-ripple touch-active">
